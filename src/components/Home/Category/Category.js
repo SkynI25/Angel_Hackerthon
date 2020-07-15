dd8ac0915da7e8a99f61707416d99a5d388f697f
dogyeong <shroad1802@naver.com>
2020-07-16 01:44:15 +0900

    add Banner Component in Home

--- src/components/Home/Category/Category.js
+++ src/components/Home/Category/Category.js
@@ -2,53 +2,51 @@ import React from 'react'
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel'
 import 'pure-react-carousel/dist/react-carousel.es.css'
 import './Category.scss'
+import { Link } from 'react-router-dom'
+
+const categories = [
+  { text: '카테고리' },
+  { text: '카테고리' },
+  { text: '카테고리' },
+  { text: '카테고리' },
+  { text: '카테고리' },
+  { text: '카테고리' },
+  { text: '카테고리' },
+]
 
 const Category = () => {
   return (
-    <div className="category-container">
+    <section className="category-container">
       <h2>어떤 메뉴를 찾으시나요?</h2>
       <CarouselProvider
-        naturalSlideWidth={125}
+        naturalSlideWidth={200}
         naturalSlideHeight={100}
         orientation="horizontal"
         totalSlides={9}
-        visibleSlides={4}
-        dragStep={4}
-        step={4}
+        visibleSlides={5}
+        dragStep={5}
+        step={5}
+        isPlaying={true}
+        interval={3000}
       >
         <Slider>
-          <Slide index={0}>
-            <div style={{ width: '100%', height: '100%', backgroundColor: 'red' }}></div>
-          </Slide>
-          <Slide index={1}>
-            <div style={{ width: '100%', height: '100%', backgroundColor: 'orange' }}></div>
-          </Slide>
-          <Slide index={2}>
-            <div style={{ width: '100%', height: '100%', backgroundColor: 'yellow' }}></div>
-          </Slide>
-          <Slide index={3}>
-            <div style={{ width: '100%', height: '100%', backgroundColor: 'green' }}></div>
-          </Slide>
-          <Slide index={4}>
-            <div style={{ width: '100%', height: '100%', backgroundColor: 'blue' }}></div>
-          </Slide>
-          <Slide index={5}>
-            <div style={{ width: '100%', height: '100%', backgroundColor: 'dodgerblue' }}></div>
-          </Slide>
-          <Slide index={6}>
-            <div style={{ width: '100%', height: '100%', backgroundColor: 'purple' }}></div>
-          </Slide>
-          <Slide index={7}>
-            <div style={{ width: '100%', height: '100%', backgroundColor: 'beige' }}></div>
-          </Slide>
-          <Slide index={8}>
-            <div style={{ width: '100%', height: '100%', backgroundColor: 'grey' }}></div>
-          </Slide>
+          {categories.map((i, idx) => (
+            <Slide tabIndex={idx} key={idx}>
+              <div className="category-item">
+                <Link className="link" to={`/category/${idx}`}>
+                  <div className="img">
+                    <span></span>
+                  </div>
+                  <span>{i.text}</span>
+                </Link>
+              </div>
+            </Slide>
+          ))}
         </Slider>
         <ButtonBack className="back-btn">&#xE000;</ButtonBack>
         <ButtonNext className="next-btn">&#xE001;</ButtonNext>
       </CarouselProvider>
-    </div>
+    </section>
   )
 }
 
